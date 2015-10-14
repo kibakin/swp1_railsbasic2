8986d1bc8e9a2040e888b3d255f79f57e9dad99c
kibakin <kibakin@snu.ac.kr>
2015-10-14 05:58:22 +0000

    first commit

---
+++ app/controllers/blog_controller.rb
@@ -0,0 +1,30 @@
+class BlogController < ApplicationController
+    def index
+        @posts = Post.all.reverse
+    end
+    
+    def create
+        Post.create(writer: params[:irum], content: params[:naeyong])
+        redirect_to :root
+    end
+    
+    def destroy
+        pp = Post.find(params[:id])
+        pp.destroy
+        redirect_to :root
+    end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+        pp = Post.find(params[:id])
+        pp.writer = params[:irum]
+        pp.content = params[:naeyong]
+        pp.save
+        redirect_to:root
+    end
+        
+    
+end
